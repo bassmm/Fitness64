7f3ee29ac413ccd6bf56808b9a66901f80012cba
tommyquacks <159901224+tommyquacks@users.noreply.github.com>
2026-03-19 12:35:39 +0000

    Update Serialization.kt

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -1,17 +1,23 @@
 package com.comp2850
 
-import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.pebble.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.pebbletemplates.pebble.loader.ClasspathLoader
-import org.jetbrains.exposed.sql.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                prettyPrint = true
+                ignoreUnknownKeys = true
+                isLenient = true
+            }
+        )
+    }
+
     routing {
         get("/json/kotlinx-serialization") {
             call.respond(mapOf("hello" to "world"))
