cf917872c1ea91a2850de2f15650e42c7d1d3131
tommyquacks <159901224+tommyquacks@users.noreply.github.com>
2026-03-19 12:33:38 +0000

    refactor(db): move user service setup into application attributes

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -1,15 +1,10 @@
 package com.comp2850
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.pebble.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.pebbletemplates.pebble.loader.ClasspathLoader
-import org.jetbrains.exposed.sql.*
+import io.ktor.util.*
+import org.jetbrains.exposed.sql.Database
+
+val UserServiceKey = AttributeKey<UserService>("userService")
 
 fun Application.configureDatabases() {
     val database = Database.connect(
@@ -18,39 +13,6 @@ fun Application.configureDatabases() {
         driver = "org.h2.Driver",
         password = "",
     )
-    val userService = UserService(database)
-    routing {
-        // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
 
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.OK)
-        }
-
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.OK)
-        }
-    }
+    attributes.put(UserServiceKey, UserService(database))
 }
