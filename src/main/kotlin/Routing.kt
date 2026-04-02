379009b25323f7052685ccecea56cfd680edd50b
GUOLIN JIANG <pgbm9486@leeds.ac.uk>
2026-04-02 12:05:05 +0000

    Add login routing and log activity page

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,20 +1,87 @@
 package com.comp2850
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.pebble.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.pebbletemplates.pebble.loader.ClasspathLoader
-import org.jetbrains.exposed.sql.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondRedirect("/login")
+        }
+
+        get("/login") {
+            call.respondTemplate("login", emptyMap())
+        }
+
+        post("/login") {
+            val params = call.receiveParameters()
+            val email = params["email"]
+            call.respondRedirect("/home")
+        }
+
+        get("/home") {
+            call.respondTemplate(
+                "home",
+                mapOf(
+                    "user" to "Sarah",
+                    "today" to "Run 5km"
+                )
+            )
+        }
+
+        get("/log") {
+            call.respondTemplate("log", emptyMap())
+        }
+
+        post("/log") {
+            call.respondRedirect("/home")
+        }
+
+        get("/quick-log") {
+            call.respondTemplate("quick-log", emptyMap())
+        }
+
+        post("/quick-log") {
+            call.respondRedirect("/log")
+        }
+
+        get("/plan") {
+            call.respondTemplate(
+                "plan",
+                mapOf(
+                    "plan" to listOf(
+                        "Monday: Run 5km",
+                        "Tuesday: Gym",
+                        "Wednesday: Rest",
+                        "Thursday: Cycling 10km",
+                        "Friday: Gym",
+                        "Saturday: Long Run",
+                        "Sunday: Rest"
+                    )
+                )
+            )
+        }
+
+        get("/progress") {
+            call.respondTemplate(
+                "progress",
+                mapOf(
+                    "stats" to "5 workouts this week",
+                    "pb" to "5km: 25min"
+                )
+            )
+        }
+
+        get("/pebble-index") {
+            call.respondTemplate(
+                "pebble-index",
+                mapOf(
+                    "user" to mapOf("name" to "John")
+                )
+            )
         }
     }
 }
