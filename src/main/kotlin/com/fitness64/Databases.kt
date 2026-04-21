f82b6021ec6b332b863461f1dca2c3919c99123d
Bassam <bassamtop123@gmail.com>
2026-04-21 18:53:36 +0100

    Update routing to use better session check fromatting to avoid repetitive code

--- src/main/kotlin/com/fitness64/Databases.kt
+++ src/main/kotlin/com/fitness64/Databases.kt
@@ -1,10 +1,8 @@
 package com.fitness64
 
-import com.fitness64.users.configureUsersRoutes
-import io.ktor.server.application.*
 import org.jetbrains.exposed.v1.jdbc.Database
 
-fun Application.configureDatabases(): Database {
+fun configureDatabases(): Database {
     val database = Database.connect(
         url = "jdbc:sqlite:./data/database.db",
         driver = "org.sqlite.JDBC"
