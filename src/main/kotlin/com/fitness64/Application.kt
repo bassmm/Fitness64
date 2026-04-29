05af118a7ba09184180aa2100158e915481bf2bd
Abdulrahman-06 <ckhk8130@leeds.ac.uk>
2026-04-29 22:51:23 +0000

    update Application.kt to include weightlifting changes and RaceService

--- src/main/kotlin/com/fitness64/Application.kt
+++ src/main/kotlin/com/fitness64/Application.kt
@@ -4,26 +4,70 @@ import com.fitness64.activities.*
 import com.fitness64.races.*
 import com.fitness64.users.*
 import io.ktor.server.application.*
+import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    // Service Init
     val database = configureDatabases()
     val userService = UserService(database)
     val activityService = ActivityService(database)
     val raceService = RaceService(database)
 
-    // Setup
+    runBlocking {
+        val weightliftingTypeId = activityService.getActivityTypeByName("Weightlifting")
+            ?: activityService.createActivityType(ActivityType("Weightlifting"))
+
+        if (activityService.getExerciseByName("Bench Press") == null) {
+            activityService.createExercise(
+                Exercise(
+                    name = "Bench Press",
+                    activityTypeId = weightliftingTypeId,
+                    category = "Chest",
+                    measurementType = "reps"
+                )
+            )
+        }
+        if (activityService.getExerciseByName("Squat") == null) {
+            activityService.createExercise(
+                Exercise(
+                    name = "Squat",
+                    activityTypeId = weightliftingTypeId,
+                    category = "Legs",
+                    measurementType = "reps"
+                )
+            )
+        }
+        if (activityService.getExerciseByName("Deadlift") == null) {
+            activityService.createExercise(
+                Exercise(
+                    name = "Deadlift",
+                    activityTypeId = weightliftingTypeId,
+                    category = "Back",
+                    measurementType = "reps"
+                )
+            )
+        }
+        if (activityService.getExerciseByName("Shoulder Press") == null) {
+            activityService.createExercise(
+                Exercise(
+                    name = "Shoulder Press",
+                    activityTypeId = weightliftingTypeId,
+                    category = "Shoulders",
+                    measurementType = "reps"
+                )
+            )
+        }
+    }
+
     configureTemplating()
     configureSerialization()
     configureSecurity(userService)
 
-    // Routing
     configureRouting(userService)
     configureUsersRoutes(userService)
-    configureActivityRoutes(activityService)
+    configureActivityRoutes(activityService, userService)
     configureRaceRoutes(raceService)
 }
